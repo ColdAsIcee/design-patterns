53deef378b938e22fd10b0a9c71a93e80464020f
ColdAsIcee <Jurlique@mail.ru>
2019-02-05 04:18:23 +0300

    fixed error

--- src/main/java/observer_WeatherData/custom/observer/Observer.java
+++ src/main/java/observer_WeatherData/custom/observer/Observer.java
@@ -2,6 +2,6 @@ package observer_WeatherData.custom.observer;
 
 public interface Observer {
 
-    void update(float temperature, float hunidity, float pressure);
+    void update(float temperature, float humidity, float pressure);
 
 }
